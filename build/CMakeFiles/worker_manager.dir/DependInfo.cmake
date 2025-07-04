053567f03c78e4fa207cd991124663346adf5657
wwwgb <116743158+wwwgb@users.noreply.github.com>
2025-07-05 02:12:00 +0800

    添加CMake版本

---
+++ build/CMakeFiles/worker_manager.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/gbw_practice/practice/worker_Manager/src/boss.cpp" "CMakeFiles/worker_manager.dir/src/boss.cpp.obj" "gcc" "CMakeFiles/worker_manager.dir/src/boss.cpp.obj.d"
+  "D:/gbw_practice/practice/worker_Manager/src/employee.cpp" "CMakeFiles/worker_manager.dir/src/employee.cpp.obj" "gcc" "CMakeFiles/worker_manager.dir/src/employee.cpp.obj.d"
+  "D:/gbw_practice/practice/worker_Manager/src/manager.cpp" "CMakeFiles/worker_manager.dir/src/manager.cpp.obj" "gcc" "CMakeFiles/worker_manager.dir/src/manager.cpp.obj.d"
+  "D:/gbw_practice/practice/worker_Manager/src/workerManager.cpp" "CMakeFiles/worker_manager.dir/src/workerManager.cpp.obj" "gcc" "CMakeFiles/worker_manager.dir/src/workerManager.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/worker_manager.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/worker_manager.dir/src/boss.cpp.obj"
+  "CMakeFiles/worker_manager.dir/src/boss.cpp.obj.d"
+  "CMakeFiles/worker_manager.dir/src/employee.cpp.obj"
+  "CMakeFiles/worker_manager.dir/src/employee.cpp.obj.d"
+  "CMakeFiles/worker_manager.dir/src/manager.cpp.obj"
+  "CMakeFiles/worker_manager.dir/src/manager.cpp.obj.d"
+  "CMakeFiles/worker_manager.dir/src/workerManager.cpp.obj"
+  "CMakeFiles/worker_manager.dir/src/workerManager.cpp.obj.d"
+  "libworker_manager.dll.a"
+  "worker_manager.exe"
+  "worker_manager.exe.manifest"
+  "worker_manager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/worker_manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
